bd45ca3a329fcfb3bebe0590c95632a91100c6a9
ChrisMeeusen <cobolstinks@gmail.com>
2014-10-25 10:45:30 -0500

    added stuffs

--- BrokerObserverPattern/src/broker/Stock.java
+++ BrokerObserverPattern/src/broker/Stock.java
@@ -1,7 +1,23 @@
 package broker;
 
 import java.util.Observable;
+import java.util.Vector;
 
 public class Stock extends Observable{
-
+	public String stockSymbol;
+	public Vector<StockStatus> statuses;
+	
+	
+	public Stock(String aStockSymbol, StockStatus aStatus){
+		statuses = new Vector<StockStatus>();
+		statuses.add(aStatus);
+		stockSymbol=aStockSymbol;
+	}
+	
+	public void addStatus(StockStatus aStatus){
+		statuses.add(aStatus);
+	}
+	
+	
+	
 }
