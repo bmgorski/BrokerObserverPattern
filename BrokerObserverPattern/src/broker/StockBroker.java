559edffb1bfe0fa19ff983d8d35b37bb37c5a0e1
bmgorski <bmgorski987@gmail.com>
2014-10-25 11:03:49 -0500

    Flushed out StockBroker

--- BrokerObserverPattern/src/broker/StockBroker.java
+++ BrokerObserverPattern/src/broker/StockBroker.java
@@ -1,13 +1,27 @@
 package broker;
 
+import java.util.HashMap;
+
 public enum StockBroker {
 	INSTANCE;
 	
 	private StockBroker() {}
+	private HashMap<String, Stock> stocks = new HashMap<String, Stock>();
+	
 	
 	public StockBroker getInstance(){
 		return INSTANCE;
 	}
 	
+	public StockStatus getCurrentStockStatus(String stockSymbol){
+		return getStock(stockSymbol).getCurrentStockStatus();
+	}
 	
+	public Stock getStock(String stockSymbol){
+		return stocks.get(stockSymbol);
+	}
+	
+	public void addStock(Stock stock){
+		stocks.put(stock.getStockSymbol(), stock);
+	}
 }
