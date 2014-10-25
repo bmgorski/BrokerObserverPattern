b78007f6b3cfeb2561e13855f61f31357f366ae5
bmgorski <bmgorski987@gmail.com>
2014-10-25 10:19:33 -0500

    examples

--- BrokerObserverPattern/src/broker/StockBroker.java
+++ BrokerObserverPattern/src/broker/StockBroker.java
@@ -4,4 +4,21 @@ public enum StockBroker {
 	INSTANCE;
 	
 	
+	private String abc;
+	
+	private StockBroker() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public void execute (String arg) {
+        // Perform operation here 
+    }
+
+	public String getAbc() {
+		return abc;
+	}
+
+	public void setAbc(String abc) {
+		this.abc = abc;
+	}
 }
